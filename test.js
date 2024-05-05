4193d6cfa298b83c310eaf6d5873681ca794b094
RushabRisal <rushabofficials@gmail.com>
2024-05-05 13:57:32 +0545

    some testings

--- test.js
+++ test.js
@@ -1,2 +1,7 @@
-const name="hello world";
-console.log(name);
+// const name="hello world";
+// console.log(name);
+const greeting=function (name){
+    console.log(`hello ${name}`);
+};
+greeting('rushab');
+greeting('harsh');
