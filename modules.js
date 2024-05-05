95c8b50ad890425700ac39e7a934088643bd1c5e
RushabRisal <rushabofficials@gmail.com>
2024-05-05 15:07:38 +0545

    concept of modules and initialization of file system

---
+++ modules.js
@@ -0,0 +1,10 @@
+//Importing the modules...
+
+// const abc=require('./people');
+// console.log(abc);
+// console.log(abc.people,abc.age);
+//destructuring....
+const {people,age}=require('./people');
+console.log(people,age);
+const os=require('os');   //os is the build-in modules which gives us the detials of our device....
+console.log(os.platform(),os.homedir());
---
+++ people.js
@@ -0,0 +1,7 @@
+const people=['rushab','harsh','rishav'];
+const age=[12,21,22,32,34];
+console.log(people);
+//exporting the value which we want to export....
+module.exports={
+    people,age
+};
