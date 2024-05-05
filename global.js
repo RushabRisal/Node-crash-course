c4b3cd6fcff46a9b48beb6c95a8a815341fb62d0
RushabRisal <rushabofficials@gmail.com>
2024-05-05 14:11:39 +0545

    Global Objects

---
+++ global.js
@@ -0,0 +1,9 @@
+//Global Objects....
+//console.log(global);
+setTimeout(() => {
+    console.log("hello world");
+    clearInterval(int);
+}, 3000);
+const int=setInterval(() => {
+    console.log('this is the interval of 2secs');
+}, 1000);
