34d524a61cfd2f7cc77b3143ec16257ab785ea4d
RushabRisal <rushabofficials@gmail.com>
2024-05-07 15:22:13 +0545

    introduced express and did some routings

--- server-client/app.js
+++ server-client/app.js
@@ -2,10 +2,11 @@ const express=require('express');
 
 //creating an express app....
 const app=express();
-
+//register view engine...
+app.set('view engine','ejs');
 //listening the request...
 app.listen(3000);
-
+//routings....
 //responding...
 app.get('/',(req,res)=>{
     //res.send('<h1>home page.</h1>');
@@ -15,3 +16,12 @@ app.get('/about',(req,res)=>{
     //res.send('<h1>about page</h1>');
     res.sendFile('./view/about.html',{root:__dirname});
 });
+
+//redirecting....
+app.get('/about-us',(req,res)=>{
+    res.redirect('/about');
+})
+//404 error...
+app.use((req,res)=>{
+    res.status(404).sendFile('./view/404.html',{root:__dirname});
+})
