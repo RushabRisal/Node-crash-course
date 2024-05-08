b14131fdc2cd7530300d796980f8c7e9d88cc7d6
RushabRisal <rushabofficials@gmail.com>
2024-05-08 18:24:01 +0545

    templates

--- server-client/app.js
+++ server-client/app.js
@@ -11,18 +11,27 @@ app.listen(3000);
 app.get('/',(req,res)=>{
     //res.send('<h1>home page.</h1>');
    // res.sendFile('./view/index.html',{root: __dirname});
-   res.render('index');
+   res.render('index',{title: 'Home'});
 });
 app.get('/about',(req,res)=>{
     //res.send('<h1>about page</h1>');
-    res.sendFile('./view/about.html',{root:__dirname});
+    //res.sendFile('./view/about.html',{root:__dirname});
+    const blogs=[
+        {title:'Rushab', snippet:'hello world!'},
+        {title:'harsh', snippet:'how are you'},
+        {title:'rishav', snippet:'im fine'}
+    ]
+    res.render('about',{title:'About',blogs});
 });
-
-//redirecting....
-app.get('/about-us',(req,res)=>{
-    res.redirect('/about');
+app.get('/blogs/create',(req,res)=>{
+    res.render('create',{title:'Create'});
 })
+//redirecting....
+// app.get('/about-us',(req,res)=>{
+//     res.redirect('/about');
+// })
 //404 error...
 app.use((req,res)=>{
-    res.status(404).sendFile('./view/404.html',{root:__dirname});
+    //res.status(404).sendFile('./view/404.html',{root:__dirname});
+    res.status(404).render('404');
 })
